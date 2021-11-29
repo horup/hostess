2afeb90e61a0eba419fd5dbd27692fedc4a7c9d6
Horup <sah@sah.dk>
2021-11-29 19:42:56 +0100

    implement tungstenite client

--- src/client/tungstenite_client.rs
+++ src/client/tungstenite_client.rs
@@ -1,17 +1,18 @@
 use std::{ sync::Arc, time::Duration};
 use futures_util::{SinkExt, StreamExt, stream::SplitSink};
-use tokio::{net::TcpStream, sync::RwLock, task::JoinHandle};
+use tokio::{net::TcpStream, sync::{Notify, RwLock}, task::JoinHandle};
 use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, connect_async, tungstenite::{Message, client::IntoClientRequest}};
 use super::{Bincoded, ClientMsg, ServerMsg};
 
 pub struct TungsteniteClient {
+    notify:Arc<Notify>,
     messages:Arc<RwLock<Vec<ServerMsg>>>,
     is_connected:Arc<RwLock<bool>>,
     reader:JoinHandle<()>,
     sink:Arc<RwLock<Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>>
 }
 
-fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMsg>>>, web_socket:String, sink:Arc<RwLock<Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>>) -> JoinHandle<()> {
+fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMsg>>>, web_socket:String, sink:Arc<RwLock<Option<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>>, notify:Arc<Notify>) -> JoinHandle<()> {
     let reader = tokio::spawn(async move {
         loop {
             while *is_connected.read().await == false {
@@ -26,6 +27,7 @@ fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMs
                                 *is_connected.write().await = true;
                                 let (s, mut stream) = ws_stream.split();
                                 *sink.write().await = Some(s);
+                                notify.notify_waiters();
 
                                 while let Some(msg) = stream.next().await {
                                     match msg {
@@ -33,6 +35,7 @@ fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMs
                                             if let Message::Binary(msg) = msg {
                                                 if let Some(msg) = ServerMsg::from_bincode(&msg) {
                                                     messages.write().await.push(msg);
+                                                    notify.notify_waiters();
                                                 } else {
                                                     break;
                                                 }
@@ -46,6 +49,7 @@ fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMs
 
                                 *is_connected.write().await = false;
                                 *sink.write().await = None;
+                                notify.notify_waiters();
                             }
                         },
                         Err(_) => {
@@ -62,18 +66,20 @@ fn spawn_reader(is_connected:Arc<RwLock<bool>>, messages:Arc<RwLock<Vec<ServerMs
 }
 
 impl TungsteniteClient {
-    /// instantiate a Client using `Tokio-Tungstenite`
+    /// instantiate a Client using `Tokio-Tungstenite` as WebSocket implementation
     ///
     /// `conn` is a websocket url, such as "ws://localhost:1234"
     pub fn new(websocket_url:&str) -> Option<Self> {
         let req = websocket_url.into_client_request();
         if let Ok(_) = req {
+            let notify = Arc::new(Notify::new());
             let is_connected = Arc::new(RwLock::new(false));
             let messages = Arc::new(RwLock::new(Vec::with_capacity(128)));
             let sink = Arc::new(RwLock::new(None));
-            let reader = spawn_reader(is_connected.clone(), messages.clone(), websocket_url.into(), sink.clone());
+            let reader = spawn_reader(is_connected.clone(), messages.clone(), websocket_url.into(), sink.clone(), notify.clone());
 
             return Some(Self {
+                notify:notify,
                 is_connected:is_connected.clone(),
                 reader:reader,
                 messages:messages,
@@ -84,16 +90,14 @@ impl TungsteniteClient {
         None
     }
 
-
-    pub fn close(&self) {
-        self.reader.abort();
-    }
-
+    /// returns true if currently connected
     pub async fn is_connected(&self) -> bool {
         let c = self.is_connected.read().await;
         return *c;
     }
 
+    /// sends a message to the server
+    /// returns true if the message was successfully sent
     pub async fn send(&mut self, msg:ClientMsg) -> bool {
         if self.is_connected().await {
             if let Some(sink) = &mut *self.sink.write().await {
@@ -107,9 +111,40 @@ impl TungsteniteClient {
         false
     }
 
-    pub async fn pop_messages(&mut self) -> Vec<ServerMsg> {
-        let cloned = self.messages.read().await.clone();
-        self.messages.write().await.clear();
-        return cloned;
+    /// gets a list of messages recieved from the server
+    /// waits for atleast one message
+    /// returns None in case of a disconnect
+    pub async fn messages(&self) -> Option<Vec<ServerMsg>> {
+        loop {
+            self.notify.notified().await;
+            if self.is_connected().await == false {
+                return None;
+            }
+
+            let messages = self.messages.read().await;
+            if messages.len() > 0 {
+                let cloned = self.messages.read().await.clone();
+                self.messages.write().await.clear();
+                return Some(cloned);
+            }
+        }
+    }
+
+    /// polls the current avaliable messages
+    /// returns None in case of a disconnect
+    pub async fn poll_messages(&mut self) -> Option<Vec<ServerMsg>> {
+        if self.is_connected().await {
+            let cloned = self.messages.read().await.clone();
+            self.messages.write().await.clear();
+            return Some(cloned);
+        }
+        
+        return None;
+    }
+}
+
+impl Drop for TungsteniteClient {
+    fn drop(&mut self) {
+        self.reader.abort();
     }
 }
